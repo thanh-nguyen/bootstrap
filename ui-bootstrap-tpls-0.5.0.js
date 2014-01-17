91669f52a6bf33e3527f8180749f40d781fe1eb9
Thanh Nguyen <nmthanh89@gmail.com>
2014-01-17 15:00:34 +0200

    revert latest change with the heading location; do not use ng-if with tabset

--- ui-bootstrap-tpls-0.5.0.js
+++ ui-bootstrap-tpls-0.5.0.js
@@ -3435,7 +3435,7 @@ angular.module("template/tabs/tabset.html", []).run(["$templateCache", function(
   $templateCache.put("template/tabs/tabset.html",
     "\n" +
     "<div class=\"tabbable\" ng-class=\"{'tabs-right': direction == 'right', 'tabs-left': direction == 'left', 'tabs-below': direction == 'below'}\">\n" +
-    "  <div tabset-titles=\"!tabsAbove\"></div>\n" +
+    "  <div tabset-titles=\"tabsAbove\"></div>\n" +
     "  <div class=\"tab-content\">\n" +
     "    <div class=\"tab-pane\" \n" +
     "         ng-repeat=\"tab in tabs\" \n" +
@@ -3443,7 +3443,7 @@ angular.module("template/tabs/tabset.html", []).run(["$templateCache", function(
     "         tab-content-transclude=\"tab\">\n" +
     "    </div>\n" +
     "  </div>\n" +
-//    "  <div tabset-titles=\"!tabsAbove\"></div>\n" +
+    "  <div tabset-titles=\"!tabsAbove\"></div>\n" +
     "</div>\n" +
     "");
 }]);
